02b117b6907799e4d5bdc60b566397d71c124365
Carlos Kelley <carloskelley@protonmail.com>
2022-11-30 21:41:16 -0600

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Carlos Kelley on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
