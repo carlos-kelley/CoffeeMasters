2e632282792a61b41224d799cc29bd707a0f28e8
Carlos Kelley <carloskelley@protonmail.com>
2022-12-01 21:21:34 -0600

    set up cart

---
+++ CoffeeMasters/View/OrderItem.swift
@@ -0,0 +1,20 @@
+//
+//  OrderItem.swift
+//  CoffeeMasters
+//
+//  Created by Carlos Kelley on 12/1/22.
+//
+
+import SwiftUI
+
+struct OrderItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderItem_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderItem()
+    }
+}
